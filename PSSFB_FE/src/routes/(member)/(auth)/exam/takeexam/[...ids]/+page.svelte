8b24fb4548c762eadd21d3a19618d7cd33552e45
justDaniel2002 <todentsukanai@gmail.com>
2024-04-20 16:52:02 +0700

    fix code

--- PSSFB_FE/src/routes/(member)/(auth)/exam/takeexam/[...ids]/+page.svelte
+++ PSSFB_FE/src/routes/(member)/(auth)/exam/takeexam/[...ids]/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import { convertSecondsToMmSs, secondsToDateTime, showToast, formatLongToDate } from '../../../../../../helpers/helpers';
+	import {
+		convertSecondsToMmSs,
+		secondsToDateTime,
+		showToast,
+		formatLongToDate
+	} from '../../../../../../helpers/helpers';
 	import { goto } from '$app/navigation';
 	import { currentUser, pageStatus } from '../../../../../../stores/store';
 	import Button from '../../../../../../atoms/Button.svelte';
@@ -8,7 +13,6 @@
 	import { initMessage } from '$lib/type';
 	import { t } from '../../../../../../translations/i18n';
 	import { page } from '$app/stores';
-
 
 	export let data;
 	const exam = data.exam;
@@ -26,42 +30,50 @@
 	};
 
 	let intervalID = setInterval(() => {
-		if(!$page.url.pathname.includes('takeexam')){
-			clearInterval(intervalID)
+		if (!$page.url.pathname.includes('takeexam')) {
+			clearInterval(intervalID);
 		}
-		
+
 		if (timeleft == 0) {
-			SubmitExam()
+			SubmitExam();
 		}
-		
+
 		timeleft = timeleft - 1;
 	}, 1000);
 
-	const SubmitExam = async() => {
-		pageStatus.set('load')
-		console.log(JSON.stringify(submitData))
-		clearInterval(intervalID)
+	const SubmitExam = async () => {
+		pageStatus.set('load');
+		console.log(JSON.stringify(submitData));
+		clearInterval(intervalID);
 		try {
-			const result:any = await submitExam(submitData)
-			if(result?.msgTextEN){
-				showToast(result.msgCode, result.msgTextEN)
+			const result: any = await submitExam(submitData);
+			if (result?.msgTextEN) {
+				showToast(result.msgCode, result.msgTextEN);
 			}
 		} catch (error) {
-			console.error(error)
+			console.error(error);
 		}
 		goto(`/exam/${courseId}/${chapterId}/${exam.id}`);
-		pageStatus.set('done')
-	}
+		pageStatus.set('done');
+	};
+
+	window.onpopstate = function (event) {
+		var result = window.confirm('Thoát trang kiểm tra?');
+		if (result) {
+		} else {
+			event.preventDefault();
+		}
+	};
 </script>
 
-<div class="px-40 py-20 ">
+<div class="px-40 py-20">
 	<div class="p-20 border rounded-xl bg-white">
 		<div class="text-center font-bold text-4xl text-blue-500">{convertSecondsToMmSs(timeleft)}</div>
 		<div class="text-3xl font-bold">{exam.name}</div>
 		<hr class="my-5" />
 		<div class="mb-5">
 			The due date for this exam is <span class="font-bold text-blue-500"
-				>{formatLongToDate(Date.now() + time*1000)}</span
+				>{formatLongToDate(Date.now() + time * 1000)}</span
 			>
 		</div>
 		{#each questionExams as qe, index}
@@ -71,6 +83,6 @@
 				<ExamAnswers bind:submitData questionExam={qe} />
 			</div>
 		{/each}
-		<div class="flex justify-end"><Button onclick={SubmitExam} content="{$t('Submit')}"/></div>
+		<div class="flex justify-end"><Button onclick={SubmitExam} content={$t('Submit')} /></div>
 	</div>
 </div>
