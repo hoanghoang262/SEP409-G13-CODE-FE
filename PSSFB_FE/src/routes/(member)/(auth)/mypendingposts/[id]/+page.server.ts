38a0790d439cbc5e5813fb5c16704fc084c3b754
hoang <hoangcanada2002@gmail.com>
2024-04-24 02:29:52 +0700

    up

--- PSSFB_FE/src/routes/(member)/(auth)/mypendingposts/[id]/+page.server.ts
+++ PSSFB_FE/src/routes/(member)/(auth)/mypendingposts/[id]/+page.server.ts
@@ -1,7 +1,7 @@
 
 import { getModPostById } from "$lib/services/ModerationServices";
 
-export async function load({params}:any){
+export async function load({ params }: any) {
     const id = params.id;
     const post = await getModPostById(id);
     return {
--- PSSFB_FE/src/routes/(member)/(auth)/mypendingposts/[id]/+page.svelte
+++ PSSFB_FE/src/routes/(member)/(auth)/mypendingposts/[id]/+page.svelte
@@ -5,14 +5,15 @@
 	import CommentContainer from '../../../../../components/CommentContainer.svelte';
 	import { showToast } from '../../../../../helpers/helpers';
 	import { currentUser } from '../../../../../stores/store';
+	import { getTimeDifference } from '../../../../../helpers/datetime';
 
-	export let data:any;
+	export let data: any;
 	const post: any = data.post;
 </script>
 
 <div class="pt-10 px-40 bg-white pb-20">
 	<div class="mb-10">Home > Forums > {post?.title}</div>
-	<div class="flex px-32">
+	<!-- <div class="flex px-32">
 		<div class="w-1/12 mr-5"><Avatar classes="rounded-full" src={post?.userPicture} /></div>
 		<div class="w-10/12">
 			<div class="text-2xl mb-3">{post?.title}</div>
@@ -22,8 +23,8 @@
 			<div class="text-xl mb-5">{post?.description}</div>
 			<div class="mb-5">
 				{@html post?.postContent}
-			</div>
-			<!-- <div class="flex justify-end text-neutral-500 text-lg">
+			</div> -->
+	<!-- <div class="flex justify-end text-neutral-500 text-lg">
 				<button
 					class="flex items-center"
 					on:click={() => {
@@ -34,13 +35,13 @@
 					}}>Share <Icon class="text-3xl" icon="mdi:share" style="color: #aeadad" /></button
 				>
 			</div> -->
-			<!-- <div class="flex">
+	<!-- <div class="flex">
                 {#each post.tag as t}
                     <div class="px-5 py-1 rounded-lg bg-neutral-100">{t}</div>
                 {/each}
             </div> -->
-		</div>
-	</div>
+	<!-- </div>
+	</div> -->
 	<!-- <div>
 		<CommentContainer
 			type="post"
@@ -49,4 +50,36 @@
 			getComment={() => getCommentByPost(post.id)}
 		/>
 	</div> -->
+	<div>
+		<p class="text-3xl">{post?.title}</p>
+		<div class="text-gray-700 text-xs mb-10">
+			<p class="ml-2">Create At: {getTimeDifference(post?.lastUpdate)}</p>
+		</div>
+	</div>
+	<div class="py-5 flex border-2 border-gray-200">
+		<div class="flex flex-col items-center px-10">
+			<p class="text-md text-center pb-1 truncate max-w-40">{post?.userName}</p>
+			<div class="w-20 h-20">
+				<Avatar classes="rounded-full w-full h-full" src={post?.userPicture} />
+			</div>
+		</div>
+		<div class="bg-gray-100 w-full px-10 mr-10 py-5 overflow-hidden">
+			<div>
+				<div class="mx-5 break-words">
+					{@html post?.postContent}
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="flex justify-end text-neutral-500 text-lg mt-3 p-1 hover:text-blue-600">
+		<button
+			class="flex items-center"
+			on:click={() => {
+				const currentUrl = window.location.href;
+				navigator.clipboard.writeText(currentUrl).then(() => {
+					showToast('URL copied to clipboard', 'URL copied to clipboard', 'info');
+				});
+			}}>Share <Icon class="text-3xl" icon="mdi:share" /></button
+		>
+	</div>
 </div>
